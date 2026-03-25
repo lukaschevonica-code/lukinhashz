3d9a5d0fe061dcb520eac7452167d3f9f0ca5561
lukaschevonica-code <lukas.chevonica@escola.pr.gov.br>
2026-03-25 18:28:20 +0000

    versao1

---
+++ produto.js
@@ -0,0 +1,15 @@
+const produto = 'Mochila Impermeável';
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessório";
+
+console.log(`O produto é : ${produto}.`);
+console.log(`A ${produto} custa R$ ${preco.toFixed(2)} `);
+console.log(`O estoque anda possiu ${estoque} é ${categoria}.`);
+
+
+
+
+
+
+
