b6e3e93d9d077868de93265812227ebc1a28694f
lukaschevonica-code <lukas.chevonica@escola.pr.gov.br>
2026-03-25 18:49:59 +0000

    versao3

--- temperatura.js
+++ temperatura.js
@@ -4,3 +4,8 @@ let tempCelsius = 18.5;
 
 const tempfahrenheit = tempCelsius * (9/5) + 32;
 const tempkelvin = tempCelsius + 237. 15;
+
+console.log(`A tempera de ${cidade} está ${tempCelsius} Cº`);
+console.log(`Fahrenheit: ${tempfahrenheit.toFixed(1)} fº`)
+console.log(`Kelvin: ${tempkelvin.toFixed(1)} Kº`)
+
