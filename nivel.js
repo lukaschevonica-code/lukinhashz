9f03888c0a300dd7ad59931d13c594a030d8d957
lukaschevonica-code <lukas.chevonica@escola.pr.gov.br>
2026-04-14 17:37:11 +0000

    v1

---
+++ nivel.js
@@ -0,0 +1,15 @@
+function subirDeNivel(nivelAtual) {
+    return nivelAtual + 1;
+}
+
+let meuNivel = 10;
+let nivelatualizado = subirDeNivel (meuNivel);
+
+console.log("seu nivel atual e " + nivelatualizado);
+
+
+
+
+
+
+
