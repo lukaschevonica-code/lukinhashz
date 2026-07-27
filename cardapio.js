0f953577dc5c81bafbe9f57e24ab439fe84f833f
lukaschevonica-code <lukas.chevonica@escola.pr.gov.br>
2026-07-27 19:22:02 +0000

    v1

---
+++ cardapio.js
@@ -0,0 +1,18 @@
+//um objeto que representa uma coisa nu mundo real
+const cardapio = [
+    {produto: "X-Burger", preco: 18.90, categoria:"lanche"},
+    {produto:"poolenta frita", preco: 10.00, categoria:"Porcoes"},
+    {produto:"laranja", preco: 10.00, categoria:"sucos"},
+    {produto:"milkshake", preco: 24.00, categoria:"sobremesa"}
+];
+
+//console.log(cardapio)
+//console.log(cardapio[2])
+//console.log(cardapio[2].produto);
+//console.log(cradapio[2].preco);
+
+//função que recebe a lista como parâmetro
+function exibircardapio(lista){
+        console.log("===CARDAPIO===")
+    console.log('1.)
+}
